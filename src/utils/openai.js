a74d6e5653a45c5a26dde54bf7f55195959bacd4
faizu01 <104008357+faizu01@users.noreply.github.com>
2024-05-06 12:16:45 +0530

    Update openai.js

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,6 +1,6 @@
 import OpenAI from "openai";
 const openai = new OpenAI({
-  apiKey: process.env.REACT_APP_OPENAI_KEY_URL, // This is the default and can be omitted
+  apiKey: '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' ,// This is the default and can be omitted
   dangerouslyAllowBrowser: true,
 });
 
